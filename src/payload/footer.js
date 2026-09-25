c74366cea787bc2655d842b8a5e00cad72c1b3ba
initbtn <min2spapa@gmail.com>
2026-09-26 01:20:05 +0900

    refactor: TypeScript 전환 후 레거시 JavaScript 파일 정리

--- src/payload/footer.js
+++
@@ -1,6 +0,0 @@
-export const footer = {
-  sign: "Sung-Ho Nam",
-  since: 2026,
-  github: "https://github.com/initbtn/resume-vuejs",
-  originalRepo: "https://github.com/uyu423/resume-nextjs"
-};
--- src/payload/profile.js
+++
@@ -1,8 +0,0 @@
-export const profile = {
-  name: "남성호",
-  position: "Front-end & Full-stack Engineer",
-  email: "min2spapa@gmail.com",
-  phone: "010-9306-2211",
-  github: "https://github.com/initbtn",
-  location: "부산광역시 사하구"
-};
